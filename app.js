1551eab5964e30ce34ea764ccd010c382630f54a
wangxqiang <729619377@qq.com>
2017-10-08 21:33:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var Bmob = require('util/bmob.js');
-Bmob.initialize("e6a3d6ddcdab9593e46dd88e7188a456", "5f03abef208dcf09dfdc3a0023530039");
+Bmob.initialize("*************************", "*************************");
 App({
   onLaunch: function () {
   },
